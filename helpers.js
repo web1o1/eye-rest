a78e52298b316db76f6850b93d44aace937b18a0
Jenn <jenn@envoy.com>
2018-11-26 22:44:36 -0800

    remove alerts

--- helpers.js
+++ helpers.js
@@ -3,16 +3,13 @@ let clearAndCreateAlarm = function(delayInMins=0, periodInMins) {
     let delayInMins = periodInMins;
   }
   let delayInMS = delayInMins * 60000;
-  alert('delay in ms: ' + delayInMS);
   chrome.storage.local.get('date', function(data) {
     chrome.alarms.get('alarmName' + data.date, function(alarm) {
       if (alarm) {
         chrome.alarms.clear('alarmName' + data.date);
       }
       chrome.alarms.create('alarmName' + data.date, {delayInMinutes: delayInMins, periodInMinutes: periodInMins});
-      chrome.storage.local.set({nextAlarmTime: Date.now()+delayInMS}, function() {
-        alert('set next alarm time: ');
-      });
+      chrome.storage.local.set({ nextAlarmTime: Date.now()+delayInMS });
     });
   });
 }
