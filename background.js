fc5a5122b6d6d98e9e7c2599eddccce7df61b3b0
Jenn <jenn@envoy.com>
2018-11-28 22:25:17 -0800

    add new images and clear alarms

--- background.js
+++ background.js
@@ -1,10 +1,19 @@
 let date = Date.now();
-let countdownMaxInMin = 20;
+let countdownMaxInMin = .3;
 let countdownMaxInSec = countdownMaxInMin * 60;
 let countdownMaxInMS = countdownMaxInSec * 1000;
 
 chrome.runtime.onInstalled.addListener(function() {
   new chrome.declarativeContent.ShowPageAction();
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {schemes: ['https', 'http', 'chrome']},
+    })],
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
 });
 
 chrome.storage.local.set({
@@ -18,7 +27,7 @@ clearAndCreateAlarm(countdownMaxInMin,countdownMaxInMin);
 // Add a listener for when the alarm is up.
 // When the alarm is up, create a window with timer.html.
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  if (alarm.name == 'alarmName' + date) {
+  if (alarm.name == 'eyeRestAlarm' + date) {
     let nextAlarmTime = alarm.scheduledTime + countdownMaxInMS;
     chrome.storage.local.set({nextAlarmTime: nextAlarmTime});
 
@@ -30,6 +39,14 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
       left: 5,
       top: 100,
       focused: true
+    });
+  } else {
+    chrome.alarms.getAll(function(data) {
+      data.forEach(function(alarm) {
+        if (alarm.name != 'eyeRestAlarm' + date) {
+          chrome.alarms.clear(alarm.name);
+        }
+      });
     });
   }
 });
--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
 let clearAndCreateAlarm = function(delayInMins, periodInMins) {
   let delayInMS = delayInMins * 60000;
   chrome.storage.local.get('date', function(data) {
-    chrome.alarms.get('alarmName' + data.date, function(alarm) {
+    chrome.alarms.get('eyeRestAlarm' + data.date, function(alarm) {
       if (alarm) {
-        chrome.alarms.clear('alarmName' + data.date);
+        chrome.alarms.clear('eyeRestAlarm' + data.date);
       }
-      chrome.alarms.create('alarmName' + data.date, {delayInMinutes: delayInMins, periodInMinutes: periodInMins});
+      chrome.alarms.create('eyeRestAlarm' + data.date, {delayInMinutes: delayInMins, periodInMinutes: periodInMins});
       chrome.storage.local.set({nextAlarmTime: Date.now()+delayInMS});
     });
   });
@@ -13,6 +13,6 @@ let clearAndCreateAlarm = function(delayInMins, periodInMins) {
 
 let clearAlarm = function() {
   chrome.storage.local.get('date', function(data) {
-    chrome.alarms.clear('alarmName' + data.date);
+    chrome.alarms.clear('eyeRestAlarm' + data.date);
   });
 }
